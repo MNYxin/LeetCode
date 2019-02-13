7eb9dbfc2bfbf73031981dc7d22b60624d184a78
MNYxin <793782876@qq.com>
2019-02-13 15:50:27 +0800

    add 22

---
+++ 22-两两交换链表中的节点.py
@@ -0,0 +1,30 @@
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+ 
+
+示例:
+
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pre,pre.next = self,head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = pre.next.next
+            pre.next,b.next,a.next = b,a,b.next
+            pre = a
+        return self.next
